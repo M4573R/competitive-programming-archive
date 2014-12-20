05411e3d83e35dd7bca2ba1ba9749fb652dd4ab5
Georgy Shopov <gsshopov@gmail.com>
2014-12-20 21:24:44 +0200

    codeforce; avoid using ceil when looking for integer division
    If you want to compute n / k such that:
    	* if n % k == 0 (n = k * m), then n / k = m
    	* if n % k != 0 and m is the biggest integer: k * m < n, then
    	  n / k = m + 1
    
    better use n / k = (n + k - 1) / k, instead of
    cast_to_integer(ceil(n / cast_to_double(k)))

--- codeforces/beta-round-1/theatre_square.cpp
+++ codeforces/beta-round-1/theatre_square.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 
 using namespace std;
@@ -13,18 +12,21 @@ int main()
 {
     use_io_iptimizations();
 
-    unsigned int theatre_square_width;
-    unsigned int theatre_square_height;
-    unsigned int flagstone_size;
+    unsigned long long theatre_square_width;
+    unsigned long long theatre_square_height;
+    unsigned long long flagstone_size;
 
     cin >> theatre_square_width >> theatre_square_height >> flagstone_size;
 
-    cout << static_cast<long long>(
-                (ceil(theatre_square_width  /
-                      static_cast<double>(flagstone_size)) *
-                 ceil(theatre_square_height /
-                      static_cast<double>(flagstone_size)))
-            );
+    unsigned long long horizontal {
+        (theatre_square_width + flagstone_size - 1) / flagstone_size
+    };
+
+    unsigned long long vertical {
+        (theatre_square_height + flagstone_size - 1) / flagstone_size
+    };
+
+    cout << horizontal * vertical << '\n';
 
     return 0;
 }
