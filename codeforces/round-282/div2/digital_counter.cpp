d29b6d67bdb5d6041d9c17d160d05efc947bb3b3
Georgy Shopov <gsshopov@gmail.com>
2014-12-21 15:10:26 +0200

    codeforces; solve "Digital Counter" from Round #282 (Div. 2)

---
+++ codeforces/round-282/div2/digital_counter.cpp
@@ -0,0 +1,24 @@
+    #include <iostream>
+    #include <vector>
+
+    using namespace std;
+
+    inline void use_io_optimizations()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+    }
+
+    int main()
+    {
+        use_io_optimizations();
+
+        vector<unsigned int> good_per_digit {2, 7, 2, 3, 3, 4, 2, 5, 1, 2};
+
+        unsigned int number;
+        cin >> number;
+
+        cout << good_per_digit[number / 10] * good_per_digit[number % 10] << '\n';
+
+        return 0;
+    }
