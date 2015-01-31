c6e1806c9a48c6d64ffe9ee5644b04bf7cc317d3
Georgy Shopov <gsshopov@gmail.com>
2015-01-31 16:58:03 +0200

    timus; resolve "K-based Numbers" with Dynamic Programming

--- timus/volume-01/k_based_numbers.cpp
+++ timus/volume-01/k_based_numbers.cpp
@@ -1,42 +1,12 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-inline void use_io_optimizations()
+inline
+void use_io_optimizations()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-}
-
-int valid_numbers(unsigned int digits,
-                  unsigned int base,
-                  bool could_end_in_zero)
-{
-    if (digits == 1)
-    {
-        return base - 1;
-    }
-
-    if (could_end_in_zero)
-    {
-        return (valid_numbers(digits - 1, base, false) +
-                (base - 1) * valid_numbers(digits - 1, base, true));
-    }
-    else
-    {
-        return (base - 1) * valid_numbers(digits - 1, base, true);
-    }
-}
-
-int valid_numbers(unsigned int digits, unsigned int base)
-{
-    if (digits == 0)
-    {
-        return 0;
-    }
-
-    return valid_numbers(digits, base, true);
 }
 
 int main()
@@ -48,7 +18,16 @@ int main()
 
     cin >> digits >> base;
 
-    cout << valid_numbers(digits, base);
+    unsigned int zero_ending     {0};
+    unsigned int non_zero_ending {base - 1};
+
+    for (unsigned int i {1}; i < digits; ++i)
+    {
+        swap(zero_ending, non_zero_ending);
+        non_zero_ending = (base - 1) * (zero_ending + non_zero_ending);
+    }
+
+    cout << zero_ending + non_zero_ending << '\n';
 
     return 0;
 }
