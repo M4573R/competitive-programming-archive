cf925203558f90b318e6add64abff9d96af567ac
Georgy Shopov <gsshopov@gmail.com>
2015-03-06 20:15:30 +0200

    peg; solve "Who is in the Middle?" from CCC 2007, Stage 1

---
+++ peg/CCC/2007/stage-1/README.md
@@ -0,0 +1,5 @@
+# 2007 Canadian Computing Competition, Stage 1
+
+* [J1: Who is in the Middle?][]
+
+[J1: Who is in the Middle?]: http://wcipeg.com/problems/desc/ccc07j1
---
+++ peg/CCC/2007/stage-1/who_is_in_the_middle.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    vector<unsigned int> weights(3);
+
+    for (auto& weight : weights)
+    {
+        cin >> weight;
+    }
+
+    sort(weights.begin(), weights.end());
+
+    cout << weights[1] << '\n';
+
+    return 0;
+}
