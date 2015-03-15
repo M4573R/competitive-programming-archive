8f1811b64c47162d602ea90c75ac22ce42cd30a1
Georgy Shopov <gsshopov@gmail.com>
2015-03-15 19:59:21 +0200

    peg; solve "Triangle Times" from CCC 2014, Stage 1

---
+++ peg/ccc/2014/stage-1/README.md
@@ -0,0 +1,5 @@
+# 2014 Canadian Computing Competition, Stage 1
+
+* [J1: Triangle Times][]
+
+[J1: Triangle Times]: http://wcipeg.com/problems/desc/ccc14j1
---
+++ peg/ccc/2014/stage-1/triangle_times.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int alpha;
+    unsigned int beta;
+    unsigned int gamma;
+
+    cin >> alpha >> beta >> gamma;
+
+    if (alpha + beta + gamma != 180)
+    {
+        cout << "Error";
+    }
+    else if (alpha == beta && beta == gamma)
+    {
+        cout << "Equilateral";
+    }
+    else if (alpha == beta || alpha == gamma || beta == gamma)
+    {
+        cout << "Isosceles";
+    }
+    else
+    {
+        cout << "Scalene";
+    }
+
+    cout << '\n';
+
+    return 0;
+}
