be9f248be1b764a36b7dc95ce7ad4fb23e54a640
Georgy Shopov <gsshopov@gmail.com>
2015-04-19 15:35:44 +0300

    codechef; solve "Super Friends" from CodeStorm 15.04

--- codechef/other/code-storm-15.04/README.md
+++ codechef/other/code-storm-15.04/README.md
@@ -4,8 +4,10 @@
 * [TWO FRIENDS AT THE ZOO][]
 * [Grading at WBUT][]
 * [PERFECT TIME][]
+* [Super Friends][]
 
 [DISTRIBUTION OF TOFFEES]: http://www.codechef.com/COST2015/problems/TOFFEES
 [TWO FRIENDS AT THE ZOO]:  http://www.codechef.com/COST2015/problems/TWOFRNDS
 [Grading at WBUT]:         http://www.codechef.com/COST2015/problems/GWBUT
 [PERFECT TIME]:            http://www.codechef.com/COST2015/problems/PERFTIME
+[Super Friends]:           http://www.codechef.com/COST2015/problems/SFRNDS
---
+++ codechef/other/code-storm-15.04/super_friends.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr unsigned int modulo {1000000007};
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+unsigned long long exponentiate(unsigned long long base,
+                                unsigned long long exponent)
+{
+    unsigned long long result    {1};
+    unsigned long long aggregate {base};
+
+    for (auto i = exponent; i > 0; i /= 2)
+    {
+        if (i % 2 == 1)
+        {
+            result = (result * aggregate) % modulo;
+        }
+
+        aggregate = (aggregate * aggregate) % modulo;
+    }
+
+    return result;
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int test_cases;
+    cin >> test_cases;
+
+    for (unsigned int test {0}; test < test_cases; ++test)
+    {
+        unsigned int sweets;
+        cin >> sweets;
+
+        if (sweets < 6)
+        {
+            cout << "FIGHT\n";
+            continue;
+        }
+
+        unsigned long long ways {1};
+
+        for (unsigned int i {sweets - 5}; i <= sweets - 1; ++i)
+        {
+            ways = (ways * i) % modulo;
+        }
+
+        cout << ways * exponentiate(120, modulo - 2) % modulo << '\n';
+    }
+
+    return 0;
+}
