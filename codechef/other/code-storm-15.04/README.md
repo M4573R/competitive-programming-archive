7e8da02c86edd56e18f6cb2c67feee522942ea79
Georgy Shopov <gsshopov@gmail.com>
2015-04-19 09:06:56 +0300

    codechef; solve "Grading at WBUT" from CodeStorm 15.04

--- codechef/other/code-storm-15.04/README.md
+++ codechef/other/code-storm-15.04/README.md
@@ -2,6 +2,8 @@
 
 * [DISTRIBUTION OF TOFFEES][]
 * [TWO FRIENDS AT THE ZOO][]
+* [Grading at WBUT][]
 
 [DISTRIBUTION OF TOFFEES]: http://www.codechef.com/COST2015/problems/TOFFEES
 [TWO FRIENDS AT THE ZOO]:  http://www.codechef.com/COST2015/problems/TWOFRNDS
+[Grading at WBUT]:         http://www.codechef.com/COST2015/problems/GWBUT
---
+++ codechef/other/code-storm-15.04/grading_at_wbut.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+unsigned long long exponentiate(unsigned long long base,
+                                unsigned long long exponent,
+                                unsigned long long modulo)
+{
+    unsigned long long result    {1};
+    unsigned long long aggregate {base};
+
+    for (auto i = exponent; i > 0; i /= 2)
+    {
+        if (i % 2 == 1)
+        {
+            result = (result * aggregate) % modulo;
+        }
+
+        aggregate = (aggregate * aggregate) % modulo;
+    }
+
+    return result;
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int test_cases;
+    cin >> test_cases;
+
+    for (unsigned int test {0}; test < test_cases; ++test)
+    {
+        unsigned int students;
+        cin >> students;
+
+        cout << exponentiate(7, students, 1000000007) << '\n';
+    }
+
+    return 0;
+}
