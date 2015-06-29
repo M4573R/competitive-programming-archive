79a07a84b1b258781ac385b3f287368a770e628e
Georgy Shopov <gsshopov@gmail.com>
2015-06-29 13:11:23 +0300

    codeforces; solve A. "Dubstep" from Round #130 (Div. 2)

---
+++ codeforces/round-130/div-2/README.md
@@ -0,0 +1,5 @@
+# Round #130 (Div. 2)
+
+* [A. Dubstep][]
+
+[A. Dubstep]: http://codeforces.com/contest/208/problem/A
---
+++ codeforces/round-130/div-2/dubstep.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    string song;
+    cin >> song;
+
+    string::size_type current {0};
+    string::size_type next    {min(song.find("WUB", current), song.size())};
+
+
+    while (current < song.size())
+    {
+        if (current != next)
+        {
+            cout << song.substr(current, next - current) << ' ';
+        }
+
+        current = next + 3;
+        next    = min(song.find("WUB", current), song.size());
+    }
+
+    cout << '\n';
+
+    return 0;
+}
