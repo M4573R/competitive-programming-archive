50c00cf17ce3ac8e61b17160234e40f4503363f2
Georgy Shopov <gsshopov@gmail.com>
2015-08-01 22:16:26 -0400

    hackerrank; solve "Class" from C++

--- hackerrank/c++/classes/README.md
+++ hackerrank/c++/classes/README.md
@@ -3,7 +3,10 @@
 | Problem statement |      Solution      |
 |:------------------|:------------------:|
 | [Structs][]       | [structs.cpp][]    |
+| [Class][]         | [class.cpp][]      |
 
 [Structs]: https://www.hackerrank.com/challenges/c-tutorial-struct
+[Class]:   https://www.hackerrank.com/challenges/c-tutorial-class
 
 [structs.cpp]: strings.cpp
+[class.cpp]:   class.cpp
---
+++ hackerrank/c++/classes/class.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    int age;
+    int standard;
+
+    string first_name;
+    string last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+
+    cout << age        << '\n'
+         << last_name  << ", "
+         << first_name << '\n'
+         << standard   << '\n';
+
+    cout << '\n';
+
+    cout << age        << ','
+         << first_name << ','
+         << last_name  << ','
+         << standard   << '\n';
+
+    return 0;
+}
