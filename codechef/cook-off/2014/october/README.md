fa5d982b2f76aed84b9c7341cb49a5227e384c35
Georgy Shopov <gsshopov@gmail.com>
2015-08-23 18:37:25 -0400

    codechef; update solution to "Mahesh and hist lost array"

--- codechef/cook-off/2014/october/README.md
+++ codechef/cook-off/2014/october/README.md
@@ -2,9 +2,10 @@
 
 * [Walk on the Axis][]
 * [The Army][]
-* [Mahesh and his lost array](http://www.codechef.com/COOK51/problems/ANUMLA)
+* [Mahesh and his lost array][]
 * [The Dirty Path](http://www.codechef.com/COOK51/problems/ANUTDP)
 * [Substring and five rules](http://www.codechef.com/COOK51/problems/ANUSFR)
 
-[Walk on the Axis]: https://www.codechef.com/COOK51/problems/ANUWTA
-[The Army]:         https://www.codechef.com/COOK51/problems/ANUARM
+[Walk on the Axis]:          https://www.codechef.com/COOK51/problems/ANUWTA
+[The Army]:                  https://www.codechef.com/COOK51/problems/ANUARM
+[Mahesh and his lost array]: https://www.codechef.com/COOK51/problems/ANUMLA
--- codechef/cook-off/2014/october/mahesh_and_his_lost_array.cpp
+++ codechef/cook-off/2014/october/mahesh_and_his_lost_array.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-inline void use_io_optimizations()
+inline
+void use_io_optimizations()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -13,31 +14,25 @@ inline void use_io_optimizations()
 
 vector<unsigned int> restore_array(const vector<unsigned int>& subset_sums)
 {
-    vector<unsigned int> array;
-
-    vector<unsigned int>            seen(subset_sums.size());
-    vector<unsigned int>::size_type seen_count {0};
-
+    vector  <unsigned int> array;
+    vector  <unsigned int> seen;
     multiset<unsigned int> expected;
 
     for (auto sum = subset_sums.cbegin() + 1; sum != subset_sums.cend(); ++sum)
     {
-        if (!expected.empty() && *expected.begin() == *sum)
+        if (!expected.empty() && *expected.cbegin() == *sum)
         {
             expected.erase(expected.begin());
         }
         else
         {
             array.push_back(*sum);
+            seen.push_back(*sum);
 
-            auto old_count     = seen_count;
-            seen[seen_count++] = *sum;
-
-            for (vector<unsigned int>::size_type i = 0; i < old_count; ++i)
+            for (unsigned int i {0}, j {seen.size()}; i + 1 < j; ++i)
             {
-                seen[seen_count] = seen[i] + *sum;
-                expected.insert(seen[seen_count]);
-                ++seen_count;
+                seen.push_back(*sum + seen[i]);
+                expected.insert(*sum + seen[i]);
             }
         }
     }
@@ -52,33 +47,26 @@ int main()
     unsigned int test_cases;
     cin >> test_cases;
 
-    for (unsigned int i {0}; i < test_cases; ++i)
+    for (unsigned int test {0}; test < test_cases; ++test)
     {
-        unsigned int array_size;
-        cin >> array_size;
+        unsigned int elements;
+        cin >> elements;
 
-        vector<unsigned int> subset_sums(1 << array_size);
+        vector<unsigned int> subset_sums(1 << elements);
 
-        for (unsigned int j {0}; j < (1 << array_size); ++j)
+        for (auto& sum : subset_sums)
         {
-            cin >> subset_sums[j];
+            cin >> sum;
         }
 
         sort(subset_sums.begin(), subset_sums.end());
 
         vector<unsigned int> array {restore_array(subset_sums)};
 
-        for (auto element = array.cbegin(); element != array.cend(); ++element)
+        for (auto i = array.cbegin(); i != array.cend(); ++i)
         {
-            cout << *element;
-
-            if (element < array.cend() - 1)
-            {
-                cout << ' ';
-            }
+            cout << *i << " \n"[i + 1 == array.cend()];
         }
-
-        cout << '\n';
     }
 
     return 0;
