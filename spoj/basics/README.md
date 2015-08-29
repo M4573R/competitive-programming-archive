1f85f51667f9539541c2dd792642b550ba6cc85a
Georgy Shopov <gsshopov@gmail.com>
2015-08-28 23:18:05 -0400

    spoj; solve 12177 - "Character Patterns (Act 2)"

--- spoj/basics/README.md
+++ spoj/basics/README.md
@@ -5,11 +5,14 @@
 | 12026 | [Test 1][]                     | [test1.cpp][]                   |
 | 12156 | [Half of the half][]           | [half_of_the_half.cpp][]        |
 | 12176 | [Character Patterns (Act 1)][] | [character_patterns_act1.cpp][] |
+| 12177 | [Character Patterns (Act 2)][] | [character_patterns_act2.cpp][] |
 
 [Test 1]:                     http://www.spoj.com/problems/TESTINT/
 [Half of the half]:           http://www.spoj.com/problems/STRHH/
 [Character Patterns (Act 1)]: http://www.spoj.com/problems/CPTTRN1/
+[Character Patterns (Act 2)]: http://www.spoj.com/problems/CPTTRN2/
 
 [test1.cpp]:                   test1.cpp
 [half_of_the_half.cpp]:        half_of_the_half.cpp
 [character_patterns_act1.cpp]: character_patterns_act1.cpp
+[character_patterns_act2.cpp]: character_patterns_act2.cpp
---
+++ spoj/basics/character_patterns_act2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+inline
+void use_io_optimizations()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    use_io_optimizations();
+
+    unsigned int test_cases;
+    cin >> test_cases;
+
+    for (unsigned int test {0}; test < test_cases; ++test)
+    {
+        unsigned int rows;
+        unsigned int columns;
+
+        cin >> rows >> columns;
+
+        for (unsigned int i {0}; i < rows; ++i)
+        {
+            for (unsigned int j {0}; j < columns; ++j)
+            {
+                if (i == 0 || i + 1 == rows || j == 0 || j + 1 == columns)
+                {
+                    cout << '*';
+                }
+                else
+                {
+                    cout << '.';
+                }
+            }
+
+            cout << '\n';
+        }
+
+        if (test + 1 < test_cases)
+        {
+            cout << '\n';
+        }
+    }
+
+    return 0;
+}
